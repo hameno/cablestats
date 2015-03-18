b48df047ceb031b1762b7cea47442b94ed8e27fd
Daniel Wandrei <cyablo@cyablo.de>
2015-03-18 08:50:28 +0100

    Timeout bei nicht verfügbaren Kanälen abfangen #2
    Exit-Code als Bedingung anstatt geparste Ausgabe.

--- freq_collect.sh
+++ freq_collect.sh
@@ -14,12 +14,8 @@ for i in "${!FREQUENCIES[@]}"; do
     fi
     timeout 5 dvbtune -f ${FREQUENCIES[$i]} -s 6950 -qam 256 > /dev/null 2>&1
     SNOOP_RESULT=`timeout 10 dvbsnoop -s bandwidth 8190 -n 3500 -hideproginfo`
-    # if [ $? -ne 0 ]; then
-    #     echo "Error during dvb snoop"
-    #     exit 1
-    # fi
-    BW=`echo ${SNOOP_RESULT} | awk -F: 'END { print $NF }' | sed 's/^[ \t]*//' | awk '{print $1}'`
-    if [ ${BW} != '15)' ]; then
+    if [ $? -eq 0 ]; then
+        BW=`echo ${SNOOP_RESULT} | awk -F: 'END { print $NF }' | sed 's/^[ \t]*/
         echo "Freq: ${FREQUENCIES[$i]} Measured: ${BW} kbit/s"
         rrdtool update ${BASE_DIR}/${FREQUENCIES[$i]}.rrd N:${BW}
     fi
